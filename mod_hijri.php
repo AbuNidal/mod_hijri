f75bc30c6c480649cfafe1eda55f7c28eb5a7fb3
Ashraf Damra <dr.d.ashraf@gmail.com>
2016-11-04 19:36:38 +0200

    Update mod_hijri.php

--- mod_hijri.php
+++ mod_hijri.php
@@ -5,7 +5,7 @@
 * Module rebuild by Dr. Ashraf Damra - Joomla! in Arabic
 * Website: https://www.jarabic.com
 * @copyright   Copyright (C) 2016 Open Source Matters, Inc. All rights reserved.
-* @license     GNU General Public License version 2 or later
+* @license     GNU General Public License version 2 or later; see LICENSE.txt
 **/
 /**************************************************************************/
 /* This program is free software. You can redistribute it and/or modify   */
